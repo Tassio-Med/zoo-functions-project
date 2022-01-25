ebc383bb6d7649df3bb1c2ac56ea0baccc85959c
Tassio Med <tassiohfmed@gmail.com>
2022-01-25 20:27:43 -0300

    requisito 3. OK

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return { };
+  }
+  return employees.find((employee) => (
+    employee.firstName === employeeName || employee.lastName === employeeName));
 }
 
 module.exports = getEmployeeByName;
