56b6368915702d4d4ed067e212b714cecdb6b0ba
Tassio Med <tassiohfmed@gmail.com>
2022-01-25 19:06:38 -0300

    requisito 2. OK.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalName = species.find((specie) => specie.name === animal);
+  return animalName.residents.every((res) => res.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
