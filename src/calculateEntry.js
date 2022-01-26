9904e6810e02771f386ce10bce371893ca51fd8a
Tassio Med <tassiohfmed@gmail.com>
2022-01-26 18:53:59 -0300

    finalmente, requisito 6, ok. AMEM

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui.
+  if (entrants.length > 0) {
+    const child = entrants.filter((element) => element.age < 18).length;
+    const adult = entrants.filter((element) => element.age >= 18 && element.age < 50).length;
+    const senior = entrants.filter((element) => element.age >= 50).length;
+
+    const obj = {
+      child,
+      adult,
+      senior,
+    };
+    return obj;
+  }
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const entries = countEntrants(entrants);
+  let totalValue = 0;
+  totalValue += prices.child * entries.child;
+  totalValue += prices.adult * entries.adult;
+  totalValue += prices.senior * entries.senior;
+  return totalValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
