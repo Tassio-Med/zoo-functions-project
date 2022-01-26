ee22278a86ed3fd9457c788ad3048a9a5eea6470
Tassio Med <tassiohfmed@gmail.com>
2022-01-26 10:30:24 -0300

    requisito 4. OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
+const { species } = data;
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(...animal) {
+  const newObj = {};
+  if (!animal[0]) {
+    species.forEach((each) => {
+      newObj[each.name] = species
+        .filter((single) => single.name === each.name)[0].residents.length;
+    });
+    return newObj;
+  }
+  const { specie, sex } = animal[0];
+  if (Object.keys(...animal).length === 1) {
+    return species.filter((single) => single.name === specie)[0].residents.length;
+  }
+
+  return species
+    .filter((single) => single.name === specie)[0].residents
+    .filter((unique) => unique.sex === sex).length;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { employees } = require('../data/zoo_data');
+
+const message = 'O id inserido não é de uma pessoa colaboradora gerente!';
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((worker) =>
+    worker.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const ifIsManager = employees.filter((worker) => worker.managers.includes(managerId));
+    return ifIsManager.map((nome) => `${nome.firstName} ${nome.lastName}`);
+  } throw new Error(message);
 }
 
 module.exports = { isManager, getRelatedEmployees };
